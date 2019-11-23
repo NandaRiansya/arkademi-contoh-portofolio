1ab0ce40fcb391b96eee28e74ffed0fd21d95e70
NandaRiansya <nandariansya67@gmail.com>
2019-11-23 14:12:35 +0700

    migration html to react

---
+++ src/landing/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import "./landing.css"
+import TopName from "./component/topName";
+import Form from "./component/form";
+import Description from "./component/description";
+import ScrollDown from './component/scrollDown';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div class="wrapper with-image ">
+                <div class="flex-wrapper">
+                    {/* topname */}
+                    <TopName
+                        name="Nanda Riansyah"
+                    ></TopName>
+                    {/* -- */}
+                    {/* description */}
+                    <Description>
+                    </Description>
+                    {/* -- */}
+                    {/* form */}
+                    <Form>
+
+                    </Form>
+                    {/* -- */}
+                    {/* scroll down */}
+                    <ScrollDown>
+                    </ScrollDown>
+                    {/* -- */}
+                </div>
+                <div class="opacity">
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Landing;
